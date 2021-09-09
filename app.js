799251812f307e7af6b6d7311d3057d57ec62d8f
blugee <blugee>
2021-09-09 03:24:49 -0400

    Changes for Pm2

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.get("/user/test", (req, res) => {
-//     res.send({ msg: "Testing" });
-// });
+app.get("/user/test", (req, res) => {
+    res.send({ msg: "Testing" });
+});
+
+app.listen(6000,console.log("Running on Port No 6000 ...."))
 console.log("Hell World!!!")
