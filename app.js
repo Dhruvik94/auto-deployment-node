999686e776f9e76990b08623f31df5e19df7fd59
blugee <blugee>
2021-09-09 02:52:26 -0400

    testing 1

---
+++ app.js
@@ -0,0 +1,7 @@
+// const express = require('express');
+// const app = express();
+
+// app.get("/user/test", (req, res) => {
+//     res.send({ msg: "Testing" });
+// });
+console.log("Hell World!!!")
